1b2c0c27b44e893bbc7b3149b8d746b8ac259f1a
AndreaRispoli97 <Andrea9715@outlook.it>
2025-02-27 12:37:27 +0100

    add: snack js sol n.3

---
+++ jsnack3/main.js
@@ -0,0 +1,12 @@
+//jsnack3
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+
+let somma = 0;
+for (let i = 0; i < 10; i++) {
+    let numero = parseFloat(prompt('Inserisci un numero'));
+    
+    somma+=numero;
+}
+
+console.log(somma);
